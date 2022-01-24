08c55fec3e2856f78cd50841d03c49e152a3daa7
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-01-24 17:29:42 +0200

    Add function, add class and ID, add icon

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const btn= document.getElementById('btn');
+const closeButton = document.getElementById('close');
+const nav= document.querySelector('.nav');
+const list = document.querySelectorAll('.common-li')
 
-const nav= document.querySelector('.nav-list');
+function closeBtn() {
+    nav.classList.remove('nav-list-menu')
+}
 
 function navList() {
-
 nav.classList.add('nav-list-menu');
 }
 
+list.forEach(element => {
+    element.addEventListener('click', closeBtn)
+})
 btn.addEventListener('click', navList);
+closeButton.addEventListener('click', closeBtn) ;
