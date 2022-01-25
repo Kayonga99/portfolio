44b6d9f2120a2c8ea614cdfbcccf157ff770421a
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-01-25 14:50:16 +0200

    Fix linter errors

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-const btn= document.getElementById('btn');
+const btn = document.getElementById('btn');
 const closeButton = document.getElementById('close');
-const nav= document.querySelector('.nav');
-const list = document.querySelectorAll('.common-li')
+const nav = document.querySelector('.nav');
+const list = document.querySelectorAll('.common-li');
 
 function closeBtn() {
-    nav.classList.remove('nav-list-menu')
+  nav.classList.remove('nav-list-menu');
 }
 
 function navList() {
-nav.classList.add('nav-list-menu');
+  nav.classList.add('nav-list-menu');
 }
 
-list.forEach(element => {
-    element.addEventListener('click', closeBtn)
-})
+list.forEach((element) => {
+  element.addEventListener('click', closeBtn);
+});
 btn.addEventListener('click', navList);
-closeButton.addEventListener('click', closeBtn) ;
+closeButton.addEventListener('click', closeBtn);
