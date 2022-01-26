1521c733fbee2c8762a924eedc04361ac56407f8
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-01-26 16:15:57 +0200

    Add snapshot photo, add github-icon, add-bgpopup class

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const btn = document.getElementById('btn');
 const closeButton = document.getElementById('close');
 const nav = document.querySelector('.nav');
 const list = document.querySelectorAll('.common-li');
+const seeProjectBtns = document.querySelectorAll('.btns')
+const popup = document.querySelector('.bgpopup')
+const closePopupBtn = document.getElementById('close-popup')
 
+console.log(closePopupBtn);
 function closeBtn() {
   nav.classList.remove('nav-list-menu');
 }
@@ -11,8 +15,22 @@ function navList() {
   nav.classList.add('nav-list-menu');
 }
 
+function openPopup() {
+  popup.style.display = 'block';
+}
+
+function closePopup() {
+  popup.style.display = 'none';
+}
+
 list.forEach((element) => {
   element.addEventListener('click', closeBtn);
 });
 btn.addEventListener('click', navList);
 closeButton.addEventListener('click', closeBtn);
+
+seeProjectBtns.forEach(element => {
+  element.addEventListener('click', openPopup )
+});
+
+closePopupBtn.addEventListener('click', closePopup)
